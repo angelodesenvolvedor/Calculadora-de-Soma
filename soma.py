66f7aabfc6070bb9afdc9568ae0c87d98eb404e1
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2024-03-13 09:12:12 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,11 @@
+# Calculadora de Soma
+
+# Solicita ao usuário para inserir dois números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma dos dois números
+soma = numero1 + numero2
+
+# Exibe o resultado
+print(f"A soma de {numero1} + {numero2} é: {soma}")
